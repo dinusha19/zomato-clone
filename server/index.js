a9d19eff4a0ca40e415ebc403a42366f3b026bd4
dinusha19 <sdinusha842@gmail.com>
2022-01-17 11:39:03 +0530

    added menu API

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ import googleAuthConfig from "./config/google.config";
 
 // microservices routes
 import Auth from "./API/Auth";
-
+import Restaurant from "./API/Restaurant";
+import Food from "./API/Food";
+import Menu from "./API/Menu";
 // Database connection
 import ConnectDB from "./database/connection";
 
@@ -32,10 +34,11 @@ googleAuthConfig(passport);
 
 // Application Routes
 zomato.use("/auth", Auth);
-
+zomato.use("/restaurant",Restaurant);
+zomato.use("/food",Food);
+zomato.use("/menu",Menu);
 
 zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
-
 
 zomato.listen(3000, () =>
   ConnectDB()
