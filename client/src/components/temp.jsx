721a47c2dcebfbf5b437aef74c3bfce6759a233a
dinusha19 <sdinusha842@gmail.com>
2022-01-30 15:47:17 +0530

    added navbar component

--- client/src/components/temp.jsx
+++ client/src/components/temp.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 function Temp (){
-    return <h1 className="text-blue-400">This is a temp componet</h1>
+    return <h1 className="text-blue-400">Temp componet</h1>
 }
 
 export default Temp
--- client/src/layouts/Home.layout.jsx
+++ client/src/layouts/Home.layout.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = (props) => {
     return (
         <>
-            <h1 className="text=xl">Home Layout</h1>
-            {props.children}
+            <div className="container mx-auto px-4 lg:px-20">
+                <Navbar/>
+                {props.children}
+                </div>
         </>
     )
 }
